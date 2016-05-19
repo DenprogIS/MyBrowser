12dd550f78fff1858def110c894326fb39a7acb1
DenprogIS <Daniyar199800@mail.ru>
2016-05-19 19:36:39 +0500

    problem with headpice

---
+++ Browser_problem_with_headpice/Browser/StringExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Browser
+{
+    public static class StringExtension
+    {
+        public static bool include(this String str, string strsPart)
+        {
+            return (str.IndexOf(strsPart) == -1) ? false : true;
+        }
+
+        public static string RemoveInsertedToBegindText(this String str, string insertedToBegindText)
+        {
+            return str.Remove(0, insertedToBegindText.Length);
+        }
+    }
+}
---
+++ Browser_problem_with_headpice/Browser/webBrowserExtension.cs
@@ -0,0 +1,25 @@
+﻿using Awesomium.Windows.Forms;
+using Awesomium.Core;
+using System;
+
+namespace Browser
+{
+    public static class webBrowserExtension
+    { 
+        public static void doScreenshot(this WebControl webBrowser, string filePath)
+        {
+            using (WebView vw = WebCore.CreateWebView(1024, 768))
+            {
+                vw.Source = new Uri(Convert.ToString(webBrowser.Source));
+
+                while (vw.IsLoading)
+                {
+                    WebCore.Update();
+                }
+
+             ((BitmapSurface)vw.Surface).SaveToJPEG(filePath + ".jpg");
+                vw.Dispose();
+            }
+        }
+    }
+}
