c3f4bc8f6a92b5928afa54b30d63bbfa7e2835fc
DenprogIS <Daniyar199800@mail.ru>
2016-05-29 00:46:58 +0500

    tab systems bug fixed

---
+++ Browser 0.7/Browser/webBrowserExtension.cs
@@ -0,0 +1,20 @@
+﻿using Awesomium.Windows.Forms;
+using Awesomium.Core;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace Browser
+{
+    public static class webBrowserExtension
+    {
+        public static void doScreenshot(this WebControl webBrowser, string fileName)
+        {
+            IWebView webView = WebCore.Views.GetByHandle(webBrowser.NativeView);
+            if (webView != null)
+            {
+                var imageSurface = webView.Surface as ImageSurface;
+                imageSurface?.Image.Save(Application.StartupPath + "\\screens\\screen" + fileName + ".jpg", ImageFormat.Jpeg);
+            }
+        }
+    }
+}
---
+++ Browser 0.7/BrowserStyle/blinds.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+namespace BrowserStyle
+{
+    /// <summary>
+    /// Логика взаимодействия для blinds.xaml
+    /// </summary>
+    public partial class blinds : UserControl
+    {
+        public blinds()
+        {
+            InitializeComponent();
+            Width = SystemParameters.VirtualScreenWidth;
+            Height = SystemParameters.VirtualScreenHeight;
+            rect.Width= SystemParameters.VirtualScreenWidth;
+            rect.Height = SystemParameters.VirtualScreenHeight;
+        }
+    }
+}
